548d030a5a3845a2d9496d86779679e2444c24bf
Rishi pal <rishipal.indolia@jungleworks.com>
2020-01-17 23:10:36 +0530

    Initial Commit

---
+++ Loc4U/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loc4U
+//
+//  Created by Rishi pal on 17/01/20.
+//  Copyright © 2020 Rishi pal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
